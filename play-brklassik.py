b0ff1584d5c1f80c58e4e4bf58dff5cfab6ea986
Dave Lambley <dave@lambley.me.uk>
2020-05-04 16:56:51 +0100

    Wait after calling play_media()

--- play-brklassik.py
+++ play-brklassik.py
@@ -1,9 +1,18 @@
 import pychromecast
 
+target_device = "Dave's Chromecast"
+
+pychromecast.IGNORE_CEC.append(target_device)  # Ignore CEC on Chromecasts named Living Room
 chromecasts = pychromecast.get_chromecasts()
 
-cast = next(cc for cc in chromecasts if cc.device.friendly_name == "Dave's Chromecast")
+cast = next(cc for cc in chromecasts if cc.device.friendly_name == target_device)
+
 cast.wait()
 print(cast.device)
 print(cast.status)
-cast.media_controller.play_media("https://br-edge-10aa-fra-dtag-cdn.cast.addradio.de/br/brklassik/live/mp3/128/stream.mp3", "audio/mp3")
+
+mc = cast.media_controller
+mc.play_media("https://br-edge-10aa-fra-dtag-cdn.cast.addradio.de/br/brklassik/live/mp3/128/stream.mp3", "audio/mp3")
+print(mc.status)
+mc.block_until_active()
+
