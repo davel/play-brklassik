1f73ef774ed52885ddf88f1f59c923bbba9a856c
Dave Lambley <dave@lambley.me.uk>
2020-12-25 13:29:52 +0000

    New hardware

--- play-brklassik.py
+++ play-brklassik.py
@@ -2,11 +2,11 @@ import sys
 import pychromecast
 import urllib
 
-target_device = "Dave's Chromecast"
+target_device = "Dave's new Chromecast"
 
 url = "https://br-brklassik-live.cast.addradio.de/br/brklassik/live/mp3/high"
 
-if len(sys.argv) > 0:
+if len(sys.argv) > 1:
     url = sys.argv[1]
 
 req_head =  urllib.request.Request(url, method="HEAD")
@@ -16,8 +16,11 @@ resp = urllib.request.urlopen(req_head)
 fmt = resp.getheader("Content-Type")
 
 pychromecast.IGNORE_CEC.append(target_device)  # Ignore CEC on Chromecasts named Living Room
-chromecasts = pychromecast.get_chromecasts()
 
+services, browser = pychromecast.discovery.discover_chromecasts()
+chromecasts, browser = pychromecast.get_listed_chromecasts(friendly_names=[target_device])
+
+print(chromecasts)
 cast = next(cc for cc in chromecasts if cc.device.friendly_name == target_device)
 
 cast.wait()
